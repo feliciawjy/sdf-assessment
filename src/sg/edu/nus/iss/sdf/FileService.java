817266958e589702c29189d87d17da472bef0623
feliciawjy <feliciawjy@gmail.com>
2024-03-01 09:23:51 +0800

    first commit

---
+++ src/sg/edu/nus/iss/sdf/FileService.java
@@ -0,0 +1,16 @@
+package sg.edu.nus.iss.sdf;
+import java.util.List;
+
+public class FileService {
+    public List<String> ReadCSV(String fullPathFilename) {
+        // Task 1 - your code here
+
+        return null;
+    }
+
+    public void writeAsCSV(String pokemons, String fullPathFilename) {
+        // Task 1 - your code here
+        System.out.println("file service compiled.");
+
+    }
+}
